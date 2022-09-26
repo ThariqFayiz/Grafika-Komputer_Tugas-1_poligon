7f6e9f210e3326c2e3d620d5d7696ad741b44ed2
Muh. Thariq Fayiz Syahputra A._E1E121070 <114406288+ThariqFayiz@users.noreply.github.com>
2022-09-26 22:01:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 void trapesium()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0.5, 0.0);
+    glColor3f(0.0, 1.0, 1.0);
 
     glBegin(GL_POLYGON);
 
@@ -41,7 +41,7 @@ void trapesium()
     glVertex2f(0.8, 0.7);   // Garis 3
     glVertex2f(0.3, 0.7);
 
-    glColor3f(0.0, 1.0, 1.0);
+    glColor3f(1.0, 0.0, 1.0);
     glVertex2f(0.3, 0.7);   // Garis 4
     glVertex2f(0.2, 0.3);
 
